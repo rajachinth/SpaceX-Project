837ee0f93e17e45332f7631d7c629f2345c95135
rajachinth <rajasekhar9040@outlook.com>
2021-02-07 02:02:37 +0530

    v3

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express=require('express');
 const app=express();
 
-app.use(express.static(__dirname+'/dist/hotlist-store'));
+app.use(express.static(__dirname+'/dist/SapientProject'));
 app.all('*',(request,response)=>{
-    response.status(200).sendFile(__dirname+'/dist/hotlist-store/index.html')
+    response.status(200).sendFile(__dirname+'/dist/SapientProject/index.html')
 });
 
 app.listen(process.env.PORT || 8080);
